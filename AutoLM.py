7ffe3cd7a8f64ff1f464b7632e1ebf4ba8ab869f
Yohann <102670831+YohannMgt@users.noreply.github.com>
2023-03-23 12:56:17 +0100

    [ADD] workflow : txt to pdf

--- AutoLM.py
+++ AutoLM.py
@@ -4,7 +4,7 @@ jobTitle = input("What is the job title: ")
 entrepriseName = input("What is the entreprise name: ")
 adress = input("What is the adress: ")
 city = input("What is the city and the postal code: ")
-file_name = "LM_" + entrepriseName + ".txt"
+file_name = entrepriseName + ".txt"
 with open('LM.txt', 'r') as f_in, open(file_name, 'w') as f_out:
     for line in f_in:
         line = line.replace('[ENTREPRISE]', entrepriseName)
