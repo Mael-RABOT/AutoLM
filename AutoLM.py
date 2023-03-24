2ab228ff73781e84fa469118a6649e408bb45139
Maël RABOT <mael.rabot@epitech.eu>
2023-03-24 09:14:52 +0100

    Base of update

--- AutoLM.py
+++ AutoLM.py
@@ -1,10 +1,23 @@
 from datetime import datetime
+import os
+import pandas as pd
+
+def check_file():
+    path = "Results"
+    if os.path.exists(path) == True:
+        print("path exist")
+    else
+        print("NO PATH")
+
+exit(0)
+
 date = datetime.today().strftime('%d/%m/%Y')
 jobTitle = input("What is the job title: ")
 entrepriseName = input("What is the entreprise name: ")
 adress = input("What is the adress: ")
 city = input("What is the city and the postal code: ")
-file_name = "LM_" + entrepriseName + ".txt"
+
+file_name = "../LM_" + entrepriseName + ".txt"
 with open('LM.txt', 'r') as f_in, open(file_name, 'w') as f_out:
     for line in f_in:
         line = line.replace('[ENTREPRISE]', entrepriseName)
